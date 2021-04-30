c8e0ec4415be3eb2fcc0e0c2edb11c706c69a2dd
Valentina Cuello <delmar_vale08@hotmail.com>
2021-04-30 14:54:34 +0000

    Style pronto

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,23 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/index.scss";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-juego">
+			<h1>Tic-Tac-Toe with React!</h1>
+			<h3>X wins the game!</h3>
+			<div className="grid-container">
+				<div className="grid-item cuadro1">X</div>
+				<div className="grid-item cuadro2">O</div>
+				<div className="grid-item cuadro3">X</div>
+				<div className="grid-item cuadro4">O</div>
+				<div className="grid-item cuadro5">X</div>
+				<div className="grid-item cuadro6">O</div>
+				<div className="grid-item cuadro7">X</div>
+				<div className="grid-item cuadro8">O</div>
+				<div className="grid-item cuadro9">X</div>
+			</div>
 		</div>
 	);
 }
